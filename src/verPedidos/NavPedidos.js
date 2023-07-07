7c79d3ce896047efa8152d8d57c51744097da6c6
Nicolas-Fabian-Sanchez <nicosanchez10@hotmail.com>
2023-07-07 00:02:48 -0300

    ultimo commit

--- src/verPedidos/NavPedidos.js
+++ src/verPedidos/NavPedidos.js
@@ -1,15 +1,15 @@
 import "./NavPedidos.css";
-//import {Link} from "react-router-dom"
+import {Link} from "react-router-dom"
 
 export default function NavPedidos(){
          return(
             <nav>
                 <h1>Estanislao Resto-Bar</h1>
                 <ul>
-                    <li className="nav"><a href="./VerPedidos">HOME</a></li>
-                    <li className="nav"><a href="../CargarMenu">CARGAR NUEVO MENU</a></li>
-                    <li className="nav"><a href="../CambiarPrecio">CAMBIAR PRECIOS MENU</a></li>
-                    <li className="nav"><a href="./">SALIR</a></li>
+                    <li className="nav"><Link to="./VerPedidos">HOME</Link></li>
+                    <li className="nav"><Link to="../CargarMenu">CARGAR NUEVO MENU</Link></li>
+                    <li className="nav"><Link to="../CambiarPrecio">CAMBIAR PRECIOS MENU</Link></li>
+                    <li className="nav"><Link to="./">SALIR</Link></li>
                 </ul>
             </nav>
          )
