7cf11057bed5b04af3ffdf4648e83289a1c7513e
Nicolas-Fabian-Sanchez <nicosanchez10@hotmail.com>
2023-07-07 00:08:13 -0300

    commit

--- src/verPedidos/NavPedidos.js
+++ src/verPedidos/NavPedidos.js
@@ -6,10 +6,10 @@ export default function NavPedidos(){
             <nav>
                 <h1>Estanislao Resto-Bar</h1>
                 <ul>
-                    <li className="nav"><Link to="./VerPedidos">HOME</Link></li>
-                    <li className="nav"><Link to="../CargarMenu">CARGAR NUEVO MENU</Link></li>
-                    <li className="nav"><Link to="../CambiarPrecio">CAMBIAR PRECIOS MENU</Link></li>
-                    <li className="nav"><Link to="./">SALIR</Link></li>
+                    <li className="nav"><Link to="/VerPedidos">HOME</Link></li>
+                    <li className="nav"><Link to="/CargarMenu">CARGAR NUEVO MENU</Link></li>
+                    <li className="nav"><Link to="/CambiarPrecio">CAMBIAR PRECIOS MENU</Link></li>
+                    <li className="nav"><Link to="/">SALIR</Link></li>
                 </ul>
             </nav>
          )
